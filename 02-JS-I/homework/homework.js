66a33838816326587fb78ca8f62508fc79f4cf5d
ALANROSO <94135869+ALANROSO@users.noreply.github.com>
2021-11-16 07:50:33 -0500

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'Hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 37;
+const nuevoNum = 38;
 
 // Crea una variable booleana:
 const nuevoBool = false;
